f9f90ea2176e9692e5f30342b1e90562de0de6b1
Leon <doncorleone142857@gmail.com>
2023-03-29 07:18:59 +0700

    first commit

---
+++ pages/hello.tsx
@@ -0,0 +1,10 @@
+export default function Hello() {
+    return (
+      <>
+        <h1 className="text-3xl font-bold underline border border-white">
+          Hello!
+        </h1>
+      </>
+    )
+  }
+  
