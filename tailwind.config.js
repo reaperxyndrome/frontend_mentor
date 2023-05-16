3c3a9d0ab0164e66d52c4bceabae6e78acf2558d
Leon <doncorleone142857@gmail.com>
2023-05-16 09:36:50 +0700

    added new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      qr_code_body: '#d6e2f0',
+      qr_code_title: '#1f3251',
+      qr_code_par: '#7b879d'
+    },
     extend: {},
   },
   plugins: [],
