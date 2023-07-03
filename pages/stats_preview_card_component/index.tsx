5d4b198119d47cf0384b511d7d3bb3d564bcde0a
Leon <doncorleone142857@gmail.com>
2023-07-04 00:56:21 +0700

    finalized change size

--- pages/stats_preview_card_component/index.tsx
+++ pages/stats_preview_card_component/index.tsx
@@ -37,7 +37,7 @@ const StatsSection = (props: {heading: string, subheading:string}) => {
 }
 function Stats(){
   return (
-    <section className='flex gap-x-14'>
+    <section className='flex gap-x-16'>
       <StatsSection heading='COMPANIES' subheading='10k+'></StatsSection>
       <StatsSection heading='TEMPLATES' subheading='314'></StatsSection>
       <StatsSection heading='QUERIES' subheading='12M+'></StatsSection>
@@ -48,17 +48,17 @@ function Stats(){
 export default function MyComponent(){
     return(
         <div className={`flex flex-col justify-center items-center h-[100vh] bg-[#090b1a] text-[white] ${inter.className}`}>
-          <div className='flex w-[1100px] h-[450px] bg-[#1b1938] rounded-lg'>
-            <section className='w-[50%] p-[4.5rem] pb-[3.75rem]'>
+          <div className='flex w-[1110px] h-[446px] bg-[#1b1938] rounded-lg'>
+            <section className='w-[100%] p-[4.5rem] pb-[3.75rem]'>
               <h1 className='text-4xl font-bold leading-tight mb-6 w-[400px]'>Get <span className="text-[#aa5cdb]">insights</span> that help your business grow.</h1>
               <p className={`w-[350px] text-[15px] leading-relaxed opacity-75 mb-[4.5rem]`}>
                 Discover the benefit of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. 
               </p>
               <Stats/>
             </section>
-            <section className='bg-[#aa5cdb] w-[50%] rounded-r-lg'>
+            <section className='bg-[#aa5cdb] min-w-[540px] min-h-[446px] rounded-r-lg'>
               {/* <div className='absolute w-[100%] h-[100%] bg-[#aa5cdb] z-10 opacity-50 bg-blend-screen'></div> */}
-              <Image className="mix-blend-multiply opacity-75 rounded-r-lg" src={HeaderImg} alt='Header Image' width={550} height={300}/>
+              <Image className="mix-blend-multiply opacity-75 rounded-r-lg" src={HeaderImg} alt='Header Image' width={540} height={446}/>
             </section>
           </div>
         </div>
