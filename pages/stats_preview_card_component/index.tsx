557fcf2e64f584522280602feea0af225f8c6276
Leon <doncorleone142857@gmail.com>
2023-07-05 23:27:33 +0700

    refactoring

--- pages/stats_preview_card_component/index.tsx
+++ pages/stats_preview_card_component/index.tsx
@@ -45,20 +45,32 @@ function Stats(){
   )
 }
 
-export default function MyComponent(){
+function ContentSection(){
+  return (
+    <section className='max-xl:flex max-xl: flex-col max-xl:items-center max-xl:justify-center max-xl:order-2 max-xl:text-center w-[100%] xl:p-[4.5rem] xl:pb-[3.75rem] max-xl:p-8'>
+      <h1 className='xl:text-4xl xl:leading-tight max-xl:text-[1.75rem] font-bold leading-tight mb-6 max-w-[400px] max-xl:w-[270px] xl:min-w-[400px]'>Get <span className="text-[#aa5cdb]">insights</span> that help your business grow.</h1>
+      <p className={`w-[350px] max-xl:w-[270px] text-[15px] leading-relaxed opacity-75 max-xl:mb-10 xl:mb-[4.5rem]`}>
+        Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. 
+      </p>
+      <Stats/>
+    </section>
+  )
+}
+
+function ImageSection(){
+  return(
+    <section className='max-xl:order-1 bg-[#aa5cdb] max-xl:max-w-[327px] xl:min-w-[540px] max-h-[446px] max-xl:rounded-t-xl xl:rounded-r-xl'>
+      <Image className="max-xl:max-w-[327px] max-xl:max-h-[240px] mix-blend-multiply opacity-75 xl:rounded-r-xl" src={HeaderImg} alt='Header Image' width={540} height={446}/>
+    </section>
+  )
+}
+
+export default function StatsPreviewCard(){
     return(
         <div className={`flex flex-col justify-center items-center h-[100vh] max-xl:h-full bg-[#090b1a] text-[white] ${inter.className}`}>
           <div className='flex max-xl:flex-col w-[77%] max-xl:max-w-[327px] xl:max-w-[1110px] xl:max-h-[446px] max-xl:my-20 bg-[#1b1938] rounded-xl'>
-            <section className='max-xl:flex max-xl: flex-col max-xl:items-center max-xl:justify-center max-xl:order-2 max-xl:text-center w-[100%] xl:p-[4.5rem] xl:pb-[3.75rem] max-xl:p-8'>
-              <h1 className='xl:text-4xl xl:leading-tight max-xl:text-[1.75rem] font-bold leading-tight mb-6 max-w-[400px] max-xl:w-[270px] xl:min-w-[400px]'>Get <span className="text-[#aa5cdb]">insights</span> that help your business grow.</h1>
-              <p className={`w-[350px] max-xl:w-[270px] text-[15px] leading-relaxed opacity-75 max-xl:mb-10 xl:mb-[4.5rem]`}>
-                Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency. 
-              </p>
-              <Stats/>
-            </section>
-            <section className='max-xl:order-1 bg-[#aa5cdb] max-xl:max-w-[327px] xl:min-w-[540px] max-h-[446px] max-xl:rounded-t-xl xl:rounded-r-xl'>
-              <Image className="max-xl:max-w-[327px] max-xl:max-h-[240px] mix-blend-multiply opacity-75 xl:rounded-r-xl" src={HeaderImg} alt='Header Image' width={540} height={446}/>
-            </section>
+            <ContentSection/>
+            <ImageSection/>
           </div>
         </div>
     )
